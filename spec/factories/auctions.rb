83691ebfbd8375b365ef35752fb9e47ab3cde5e7
anthonygharvey <anthony@anthonygharvey.com>
2019-03-17 19:28:34 -0400

    Added Auction model

---
+++ spec/factories/auctions.rb
@@ -0,0 +1,8 @@
+FactoryBot.define do
+  factory :auction do
+    start_date { "2019-03-17 19:27:34" }
+    end_date { "2019-03-17 19:27:34" }
+    title { "MyString" }
+    description { "MyText" }
+  end
+end
