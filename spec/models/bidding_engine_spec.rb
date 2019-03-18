0d38cb989c603ce5278f89f927ce0625d095dedd
anthonygharvey <anthony@anthonygharvey.com>
2019-03-17 22:19:20 -0400

    Refactored BiddingEngine tests

--- spec/models/bidding_engine_spec.rb
+++ spec/models/bidding_engine_spec.rb
@@ -2,34 +2,25 @@ require 'rails_helper'
 
 RSpec.describe BiddingEngine do
   describe ".bid!" do
-    it "create a new bid on an auction" do
-      seller = User.create(:email => 'jane@doe.com', :password => 'pw1234')
-      bidder = User.create(:email => 'john@doe.com', :password => 'pw1234')
+    let(:seller) { User.create(:email => 'jane@doe.com', :password => 'pw1234') }
+    let(:bidder) { User.create(:email => 'john@doe.com', :password => 'pw1234') }
+		let(:auction) { Auction.create(title: 'Anything', description: 'Lorem ipsum', 
+			start_date: DateTime.now, end_date: DateTime.now + 1.week, seller: seller) }
 
-			auction = Auction.create(title: 'Anything', description: 'Lorem ipsum', 
-				start_date: DateTime.now, end_date: DateTime.now + 1.week, seller: seller)
-
+    it "create a new bid on an auction if bid is bigger than last bid on auction" do
       described_class.bid!(auction, 100, bidder)
       expect(auction.errors).to be_empty
 
       described_class.bid!(auction, 90, bidder)
       expect(auction.errors[:bid].first).to eq "must be bigger than the last bid on the auction"
-		end
-		
-		it 'cannot create a bid if it\'s an equal amount as the last bid' do
-  		seller = User.create(:email => 'jane@doe.com', :password => 'pw1234')
-			bidder = User.create(:email => 'john@doe.com', :password => 'pw1234')
+    end
 
-			auction = Auction.create(title: 'Anything', description: 'Lorem ipsum',
-						start_date: DateTime.now, end_date: DateTime.now + 1.week,
-						seller: seller)
+    it 'cannot create a bid if it\'s an equal amount as the last bid' do
+      described_class.bid!(auction, 100, bidder)
+      expect(auction.errors).to be_empty
 
-			described_class.bid!(auction, 100, bidder)
-			expect(auction.errors).to be_empty
-
-			described_class.bid!(auction, 100, bidder)
-			expect(auction.errors[:bid].first).to eq "must be bigger than the last bid on the auction"
-		end
-
+      described_class.bid!(auction, 100, bidder)
+      expect(auction.errors[:bid].first).to eq "must be bigger than the last bid on the auction"
+    end
   end
 end
